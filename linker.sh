400120176e60bcecbd39e85eb59a358c11154fad
yasu80 <p111026h@mail.kyutech.jp>
2019-01-07 04:24:24 +0900

    LateXWorkshop日本語化@vscode
    https://qiita.com/ymfj/items/088fa556c94fc9ab460f
    https://gist.github.com/Ikuyadeu/204d06fffd912f441b383eb02463e29b

--- linker.sh
+++ linker.sh
@@ -2,4 +2,6 @@
 ln -si ~/dotfiles/.vimrc ~/.vimrc
 ln -si ~/dotfiles/.bashrc ~/.bashrc
 ln -si ~/dotfiles/.vim/ ../.vim
-ln -si ~/dotfiles/latexmkrc ../.latexmkrc
+ln -si ~/dotfiles/.latexmkrc ../.latexmkrc
+ln -si ~/dotfiles/settings.json /home/yasu/.config/Code/User/settings.json
+ 
